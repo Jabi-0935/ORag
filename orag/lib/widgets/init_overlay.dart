d123b4212378dd4454e49239fffee431aa611e92
Contributor <contributor@example.com>
2026-04-23 09:24:48 +0530

    fix: missing bracket in init_overlay.dart causing build failure

--- orag/lib/widgets/init_overlay.dart
+++ orag/lib/widgets/init_overlay.dart
@@ -144,6 +144,7 @@ class _InitOverlayState extends State<InitOverlay>
                         ),
                       ),
                     ),
+                  ],
                 ],
               ),
             ],
